d73aa9aa47128278e5999753deac66a563b8713f
Amanpreet Singh <apsdehal@gmail.com>
2014-11-16 04:36:22 +0530

    Adds test for err

--- tests/test.js
+++ tests/test.js
@@ -5,9 +5,7 @@ var mp3Length = require('../index.js');
 describe('mp3Length()', function () {
     it('should return a correct value for length', function (done) {
         mp3Length('./tests/demo.mp3', function (err, length) {
-            if (err) {
-                throw err;
-            }
+            assert.equal(err,null);
             assert.equal(length, 7);
             done();
         });
